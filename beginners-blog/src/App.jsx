0ba12cb96d44863e6c02844c55cb65980c375005
alexisdelgad113 <99755269+alexisdelgad113@users.noreply.github.com>
2022-12-14 19:10:33 -0500

    This is the blog

--- beginners-blog/src/App.jsx
+++ beginners-blog/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Blog from './pages/Blog';
 import Home from './pages/Home';
 
+
 const App = () => {
   return (
     <div className='container'>
--- beginners-blog/src/components/Home/Header/index.jsx
+++ beginners-blog/src/components/Home/Header/index.jsx
@@ -3,13 +3,12 @@ import './styles.css';
 
 const Header = () => (
   <header className='home-header'>
-    <h2>Inc. This Morning</h2>
+    <h2>Chef's Kiss</h2>
     <h1>
       <span>“</span> Blog <span>”</span>
     </h1>
     <p>
-      awesome place to make oneself <br /> productive and entertained through
-      daily updates.
+      Wabash College Senior Capstone CSC <br /> (The best food blog on the planet)
     </p>
   </header>
 );
